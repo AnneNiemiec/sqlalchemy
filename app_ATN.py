7342ecfe40e44c7926fe375fab4a16237fdf6f42
Anne Niemiec <anne_niemiec@outlook.com>
2021-05-18 12:05:13 -0700

    Climate HW complete and initial code for app HW ready

--- app_ATN.py
+++ app_ATN.py
@@ -1,5 +1,5 @@
 # 1. import Flask
-from flask import Flask
+from flask import Flask,jsonify
 
 import numpy as np
 import pandas as pd
@@ -33,26 +33,13 @@ session=Session(engine)
 # Create an app, being sure to pass __name__
 app = Flask(__name__)
 
-
 # Define what to do when a user hits the index route
 @app.route("/api/v1.0/precipitation")
+
 def last_year_prcp():
-    print("Server received request for 'Home' page...")
-    return "Welcome to my 'Home' page!"
+    query_date= dt.date(2017,8,23) - dt.timedelta(days=365)
+    recent_data=session.query(Measurement.prcp, Measurement.date).filter(Measurement.date >= query_date).all()
+    myDict = {date:prcp for date,prcp in recent_data}
+    return jsonify(myDict)
 
-
-# Define what to do when a user hits the /about route
-@app.route("/about")
-def about():
-    print("Server received request for 'About' page...")
-    return "Welcome to my 'About' page!"
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
-# %matplotlib inline
-# from matplotlib import style
-# style.use('fivethirtyeight')
-# import matplotlib.pyplot as plt
 
